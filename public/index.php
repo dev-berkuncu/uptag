5187ecd6b855b4fd744e6367513db9d378089347
BeeYou97 <berkuncu41@hotmail.com>
2025-12-29 03:15:05 +0300

    Update branding to Sociaera on index.php

--- public/index.php
+++ public/index.php
@@ -12,8 +12,8 @@ require_once '../includes/ads_logic.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Uptag ile favori mekanlarında check-in yap, puan kazan ve haftalık liderlik tablosunda yerini al!">
-    <title>Uptag - Mekanlarını Keşfet, Check-in Yap!</title>
+    <meta name="description" content="Sociaera ile favori mekanlarında check-in yap, puan kazan ve haftalık liderlik tablosunda yerini al!">
+    <title>Sociaera - Mekanlarını Keşfet, Check-in Yap!</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
